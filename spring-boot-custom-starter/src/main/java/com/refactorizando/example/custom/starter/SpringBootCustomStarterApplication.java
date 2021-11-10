dac6dd6fb522bdf90d2cbecb3db0ef36431144e7
refactorizando <refactorizando.web@gmail.com>
2021-11-10 21:42:52 +0100

    rename files

--- spring-boot-custom-starter/src/main/java/com/refactorizando/example/custom/starter/SpringBootCustomStarterApplication.java
+++ spring-boot-custom-starter/src/main/java/com/refactorizando/example/custom/starter/SpringBootCustomStarterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringRestDemoApplication {
+public class SpringBootCustomStarterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringRestDemoApplication.class, args);
+		SpringApplication.run(SpringBootCustomStarterApplication.class, args);
 	}
 
 }
